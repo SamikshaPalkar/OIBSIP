9152fa78b0bb7559f8d36dbcded9ec43897314ff
Samiksha Palkar <136262751+SamikshaPalkar@users.noreply.github.com>
2023-07-30 13:16:29 +0530

    Add files via upload

---
+++ QuizApp/src/main/java/com/example/quizapp/QueAns.java
@@ -0,0 +1,30 @@
+package com.example.quizapp;
+
+public class QueAns {
+
+    public static String question[] = {
+            "For which of the following Android is mainly developed?",
+            "Android is based on which of the following language?",
+            "What does API stand for?",
+            "What does APK stands for?",
+            "What is an activity in android?"
+    };
+
+    public static String choices[][] = {
+            {"Servers", "Desktops", "Laptops", "Mobile devices"},
+            {"Java", "C++", "C", "None of the above"},
+            {"Application Programming Interface", "Android Programming Interface", "Android Page Interface", "Application Page Interface"},
+            {"Android Phone Kit", "Android Page Kit", "Android Package Kit", "None of the above"},
+            {"android class", "android package", "A single screen in an application with supporting java code", "None of the above"}
+
+    };
+
+    public static String correctAnswer[] = {
+            "Mobile devices",
+            "Java",
+            "Application Programming Interface",
+            "Android Package Kit",
+            "A single screen in an application with supporting java code"
+
+    };
+}
